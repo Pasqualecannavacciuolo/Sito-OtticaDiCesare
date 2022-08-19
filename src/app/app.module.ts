6bb1599c313b35621a8ecd245bc8dc38b369ffce
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2022-08-19 10:44:46 +0200

    Reorganized folders structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LastAddedComponent } from './last-added/last-added.component';
+import { LastAddedComponent } from './carousel/last-added/last-added.component';
 
 // Module for custom slideshow
 import { SwiperModule } from 'swiper/angular';
-import { SlideshowComponent } from './slideshow/slideshow.component';
+import { SlideshowComponent } from './carousel/slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LastAddedComponent,
-    SlideshowComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule,
