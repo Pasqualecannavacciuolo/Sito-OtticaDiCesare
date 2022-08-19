17795bf1ffd94cb42313f13953938e899fe2dcf4
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2022-08-19 13:04:52 +0200

    Improvement animation on open & close the menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,23 +14,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("** APP COMPONENT AVVIATO **");
-    this.openAndCloseMobileMenu()
-  }
-
-  openAndCloseMobileMenu() {
-   let navbarToggler = document.getElementById('navbar-toggler');
-   let navbarSupportedContent = document.getElementById('navbarSupportedContent');
-   let navLink = document.querySelectorAll('.nav-link')
-
-   // Per ogni elemento del menu metto in ascolto su un click per chiudere in automatico il menu
-   navLink.forEach(element => {
-    element.addEventListener('click', () => {
-      setTimeout(() => {
-        navbarToggler?.classList.add('collapsed')
-        navbarSupportedContent?.classList.remove('show')
-      }, 150);
-    });
-   });
   }
 
 }
